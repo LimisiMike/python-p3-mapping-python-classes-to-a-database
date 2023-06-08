932033e707df77dd77810e86831db9bee114b936
LimisiMike <michaellimisi7@gmail.com>
2023-06-08 09:05:30 +0300

    Completed assignment

--- lib/config.py
+++ lib/config.py
@@ -1,4 +1,4 @@
 import sqlite3
 
-CONN = sqlite3.connect('music.db')
-CURSOR = CONN.cursor()
+CONN = sqlite3.connect('music.db') #is equal to a hash that contains our connection to the database
+CURSOR = CONN.cursor() #Allows us to interact with the database
